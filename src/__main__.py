458eaa1f74d139d13a33f5abce8a05af2032f2f6
tom <thomas@Laptop-Niglo.localdomain>
2026-09-25 11:22:30 +0200

    git_all

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,25 @@
 import fire
 
 class RagCLI:
-    pass
+    def index(max_chunk_size: int = 2000) -> None:
+        pass
+
+    def search(query: str, k: int = 5) -> None:
+        pass
+
+    def search_dataset(dataset_path: str, save_dir: str, k: int) -> None:
+        pass
+    
+    def answer(query: str, k: int) -> None:
+        pass
+    
+    def answer_dataset(student_search_results_path: str,
+                       save_directory: str) -> None:
+        pass
+    
+    def evaluate(student_search_results_path: str,
+                 dataset_path: str, k: int) -> None:
+        pass
 
 if __name__ == "__main__":
     # Launch CLI
